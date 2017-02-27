6d6e7c0f39d5073d97e5a9a51fbeb4c1e1ca672d
Sierra <sierramroberts.96@gmail.com>
2017-02-27 16:04:33 -0500

    Alot

--- dashboard.js
+++ dashboard.js
@@ -1,17 +1,76 @@
 // 1. Count the number of records in the "Current Quarter Details" table.
+var thing = document.getElementsByTagName('tr').length; 
 
+console.log(thing - 1);
 
 // 2. Set each dashboard image to different images, one for each season. Make sure you also set the alt="" attribute with an appropriate description.
 
+var winterImage = document.getElementsByTagName('img')[0];
+
+winterImage.src = 'http://unsplash.it/400?image=730';
+
+var springImage = document.getElementsByTagName('img')[1];
+
+springImage.src = 'http://unsplash.it/400?image=696';
+
+var summerImage = document.getElementsByTagName('img')[2];
+
+summerImage.src = 'http://unsplash.it/400?image=689';
+
+var fallImage = document.getElementsByTagName('img')[3];
+
+fallImage.src = 'http://unsplash.it/400?image=776';
 
 // 3. Below each dashboard image, there's a season/quarter name. Below those, there are three dots. Replace those three dots with a randomly generated number formatted as currency, like $12,589.00.
 
+var numberZero = document.querySelectorAll('.text-muted')[0];
+
+numberZero.innerText = '$12,589';
+
+// console.log('$' + numberZero);
+
+var numberOne = document.querySelectorAll('.text-muted')[1];
+
+numberOne.innerText = '$300';
+
+// console.log('$' + numberOne);
+
+var numberTwo = document.querySelectorAll('.text-muted')[2];
+
+numberTwo.innerText = '$543';
+
+console.log('$' + numberTwo);
+
+var numberThree = document.querySelectorAll('.text-muted')[3];
+
+numberThree.innerText = '$1,738';
+
+console.log('$' + numberThree);
 
 // 4. Change the currently selected left side menu item from Overview to Reports.
+
+// ****var changeReports = document.querySelectorAll('#');
+
+// changeReports.class = 'sr-only';****
+
+// *****var overviewToReports = document.querySelectorAll('href')[1];
+
+// overviewToReports.class = 'sr-only';
+
+// var overviewToReport = document.getElementsByTagName('ul');
+
+var overviewToReport = document.querySelectorAll('.nav-sidebar > li');
+overviewToReport[0].classList.remove("active"); 
+overviewToReport[1].classList.add("active");
 
 
 // 5. Set the default search input value to "Q4 sales".
 
+var inputValue = document.querySelectorAll('.form-control');
+
+// var x = inputValue.setAttribute("placeholder", "Q4 Sales");
+console.log(inputValue)
+// jeff did forEach(); 
 
 // 6. Add the "table-hover" class to the table. See the Bootstrap table docs.
 
